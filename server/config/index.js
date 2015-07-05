5bb49fda6b5c3f261dbb8eb5ffae9149faab3a39
bob <bob.yuxinyang@gmail.com>
2015-07-05 18:17:48 +0800

    add wechat server config

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,8 @@
 var config = {
   port:  process.env.NODE_ENV == 'production' ? 7777 : 3000,
   wechat: {
-    appid: 'wx7544dba9d7a2a7d9',
-    secret: 'd3895138041a24bd4c26e1999b548518',
+    appid: 'wxe69304ede4a1d74c',
+    secret: '149c1133385f54cf1080d88b9bde3184',
   }
 }
 
