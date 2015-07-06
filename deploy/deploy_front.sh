0b1b71f09714ff23d679ee8996ad30c9d0e4d8e4
bob <bob.yuxinyang@gmail.com>
2015-07-06 21:58:39 +0800

    update deploy

--- deploy/deploy_front.sh
+++ deploy/deploy_front.sh
@@ -3,20 +3,21 @@
 set timeout -1
 
 # inner server  xinzhaitong-admin-1
-spawn scp -r ./xuexh5-website.zip freefcw@76.74.178.79:/data/www/xuexh5
+#spawn scp -r ./xuexh5-website.zip freefcw@76.74.178.79:/data/www/xuexh5
+spawn scp -r ./xuexh5-website.zip root@121.42.11.68:/data/www/xuexh5
 expect "*assword:*"
-send "Xinzhaitongdev007\r"
+send "iampangxieDEV002\r"
 interact
 
-spawn ssh freefcw@76.74.178.79
+spawn ssh root@121.42.11.68
 expect "*assword:*"
-send "Xinzhaitongdev007\r"
+send "iampangxieDEV002\r"
 
 send "cd /data/www/xuexh5\r"
 
 send "rm -rf ./dist\r"
 send "unzip xuexh5-website.zip -d ./dist\r"
 
-send "rm xuexh5-website.zip\r"
+send "rm -rf xuexh5-website.zip\r"
 send "exit\r"
 interact
